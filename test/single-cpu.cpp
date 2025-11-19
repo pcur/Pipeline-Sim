8c4d2481d6f2a6c2e732a77374928d5404871053
Joshua <jkauffm4@charlotte.edu>
2025-11-18 20:21:55 -0500

    Updated single-cpu.cpp

--- test/single-cpu.cpp
+++ test/single-cpu.cpp
@@ -3,13 +3,22 @@
 #include "../sim/memory.h"
 
 int main(){
-
+    uint32_t tempQ[38];
+    uint32_t i;
+    uint32_t memAddr;
     //instance basic memory bank
     Memory memBank = Memory();
     pipelineSimulation cpuSim;
-    cpuSim = pipelineSimulation();
-    //load data/instructions
     //instance pipeline sim
+    cpuSim = pipelineSimulation();
+    //load data/instructions into memory
+    fill_queue("new_instructions.txt", tempQ, 38);
+    for(i=0;i<38;i++){
+        memAddr = i * 4;
+        memBank.storeWord(memAddr, tempQ[i]);
+    }
+    //Run the sim
+    cpuSim.run();
     //run some tests/printouts
     //calculations?
     return 0;
