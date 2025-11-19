d3706e18819923bdd63fe8c06aaba16100fea28b
Patrick Cur <pcur@charlotte.edu>
2025-11-19 10:33:21 -0500

    add fill_queue to single and dual tests

--- test/single-cpu.cpp
+++ test/single-cpu.cpp
@@ -2,6 +2,19 @@
 #include "../sim/pipeline.h"
 #include "../sim/memory.h"
 
+
+void fill_queue(const std::string& filename, unsigned int* instructionQueue, size_t queueSize) {
+    std::ifstream infile(filename);
+    std::string line;
+    size_t index = 0;
+
+    while (std::getline(infile, line) && index < queueSize) {
+        unsigned int instruction = std::bitset<32>(line).to_ulong();
+        instructionQueue[index] = instruction;  // Store instruction in array
+        ++index;  // Move to the next index
+    }
+}
+
 int main(){
     uint32_t tempQ[38];
     uint32_t i;
