5127d5266d7477dcc3d140e09ea77a580df39d7a
Patrick Cur <pcur@charlotte.edu>
2025-11-20 01:30:30 -0500

    add cpi calculation to demo scripts

--- test/dual-cpu.cpp
+++ test/dual-cpu.cpp
@@ -6,7 +6,8 @@ int debug;
 
 int main(){
     debug = 1;
-    unsigned int instrQ[10];
+    unsigned int instrQ0[38];
+    unsigned int instrQ1[38];
     printDebug("Setting up Pipeline test...", 0);
     printDebug("Initializing MemoryBus", 1);
     MemoryBus memBus = MemoryBus(0x00FF, 0x01FF, 0x13FF);
@@ -16,10 +17,10 @@ int main(){
 
     // Load instruction queue from file and into memory
     printDebug("Setting up instruction queue", 1);
-    fill_queue("instructions/cpu0_instructions.txt", instrQ, 38);
-    load_mem_array(memBus, 0x0000, 0x0093, instrQ);
-    fill_queue("instructions/cpu1_instructions.txt", instrQ, 38);
-    load_mem_array(memBus, 0x0100, 0x0193, instrQ);
+    fill_queue("instructions/cpu0_instructions.txt", instrQ0, 38);
+    load_mem_array(memBus, 0x0000, 0x0093, instrQ0);
+    fill_queue("instructions/cpu1_instructions.txt", instrQ1, 38);
+    load_mem_array(memBus, 0x0100, 0x0193, instrQ1);
 
         //Populate data vectors in memory with random FP32 values
     //0x400-0x7FF (ARRAY_A)
@@ -41,13 +42,28 @@ int main(){
     printDebug("============================================================", 0);
     pipeline1.start();
     pipeline2.start();
-    int loop = 0;
+    unsigned int tick0 = 0;
+    unsigned int tick1 = 0;
     while(!pipeline1.halted || !pipeline2.halted){
-        if(!pipeline1.halted) pipeline1.tick();
-        if(!pipeline2.halted) pipeline2.tick();
+        if(!pipeline1.halted) {
+            pipeline1.tick();
+            tick0++;
+        }
+        if(!pipeline2.halted) {
+            pipeline2.tick();
+            tick1++;
+        }
         memBus.tick(); //advance memory bus arbitration
-        loop++;
     }
-    printDebug("CPU simulation complete", 0);
+    unsigned int cycles0 = tick0 / 10;
+    unsigned int cycles1 = tick1 / 10;
+    unsigned int instructionCt0 = sizeof(instrQ0) / sizeof(instrQ0[0]);
+    unsigned int instructionCt1 = sizeof(instrQ1) / sizeof(instrQ1[0]);
+    float calculated_cpi0 = cycles0 / instructionCt0;
+    float calculated_cpi1 = cycles1 / instructionCt1;
+    printDebug("CPU simulation complete.", 0);
+    printDebug("CPU0 CPI: " + std::to_string(calculated_cpi0), 0);
+    printDebug("CPU1 CPI: " + std::to_string(calculated_cpi1), 0);
+    
     return 0;
 }
--- test/single-cpu.cpp
+++ test/single-cpu.cpp
@@ -6,7 +6,7 @@ int debug;
 
 int main(){
     debug = 1;
-    uint32_t tempQ[38];
+    uint32_t instrQ0[38];
     printDebug("Setting up Pipeline test...", 0);
     printDebug("Initializing MemoryBus", 1);
     MemoryBus memBus = MemoryBus(0x00FF, 0x01FF, 0x13FF);
@@ -15,8 +15,8 @@ int main(){
 
     // Load instruction queue from file and into memory
     printDebug("Setting up instruction queue", 1);
-    fill_queue("instructions/cpu0_instructions.txt", tempQ, 38);
-    load_mem_array(memBus, 0x0000, 0x0093, tempQ);
+    fill_queue("instructions/cpu0_instructions.txt", instrQ0, 38);
+    load_mem_array(memBus, 0x0000, 0x0093, instrQ0);
 
     // Initialize pipeline simulations
     printDebug("Creating pipeline simulations", 1);
@@ -26,14 +26,18 @@ int main(){
     printDebug("Starting CPU simulation loop", 0);
     printDebug("============================================================", 0);
     pipeline1.start();
-    int loop = 0;
+    int tick0 = 0;
     while(!pipeline1.halted){
         if(!pipeline1.halted) pipeline1.tick();
         memBus.tick(); //advance memory bus arbitration
-        loop++;
-        if (loop >= 100) break;
+        tick0++;
+        if (tick0 >= 100) break;
         }
 
-    printDebug("CPU simulation complete", 0);
+    unsigned int cycles0 = tick0 / 10;
+    unsigned int instructionCt0 = sizeof(instrQ0) / sizeof(instrQ0[0]);
+    float calculated_cpi0 = cycles0 / instructionCt0;
+    printDebug("CPU simulation complete.", 0);
+    printDebug("CPU0 CPI: " + std::to_string(calculated_cpi0), 0);
     return 0;
 }
